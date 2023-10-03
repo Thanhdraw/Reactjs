2664297e7047854d52a6b3cfff1bb6386d51eca6
dangquocthanh <dangquocthanh2812@gmail.com>
2023-10-03 22:47:13 +0700

    xong intro

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  // ...
+};

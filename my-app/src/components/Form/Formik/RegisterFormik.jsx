cebecae8318ffe88fe6d921553f27c12427cb335
thanhdang2812 <dangquocthanh2812@gmail.com>
2023-11-22 14:59:32 +0700

    159

--- my-app/src/components/Form/Formik/RegisterFormik.jsx
+++ my-app/src/components/Form/Formik/RegisterFormik.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import InputHookFormik from "../input/InputFormik";""
 const RegisterFormik = () => {
   return (
     <form className="max-w-[300px] mx-auto my-10">
@@ -7,7 +7,19 @@ const RegisterFormik = () => {
         <label htmlFor="username" className="cursor-pointer">
           UserName
         </label>
+        <InputHookFormik 
+          name="username"
+          placeholder="Enter your username"
+          id="username"
+          type="text"></InputHookFormik>
+         <p className="text-red-500 text-sm">Your errors message</p>
+
       </div>
+      <button
+        className="w-full p-5 bg-blue-500 text-white rounded-lg mt-5 font-semibold"
+      >
+        Submit
+      </button>
     </form>
   );
 };
---
+++ my-app/src/components/Form/input/InputFormik.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InputHookFormik = ()=>{
+    return (
+        <input className="p-4 border border-gray-100 rounded-lg bg-white outline-none transition-all focus:border-blue-500 text-black"
+        >
+
+        </input>
+    );
+};
+
+export default InputHookFormik;
